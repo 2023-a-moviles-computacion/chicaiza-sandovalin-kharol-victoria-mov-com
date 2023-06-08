5203da3d547d8c8fd1940531f106881849117a41
Kharol Chicaiza <kharol.chicaiza@epn.edu.ec>
2023-06-08 16:15:45 -0500

    start -android studio

--- 01-Android/app/src/main/java/com/example/movilescomp2023a/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/movilescomp2023a/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.movilescomp2023a
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// en AppCompatActivity va a estar toda la logica que vemos aqui
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun irActividad(
+    clase: Class<*>
+    ){
+        val intent=Intent(this,clase)
+        startActivity(intent)
     }
 }
